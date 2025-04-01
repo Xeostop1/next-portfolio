e5c4d36ff490414d342b8302856dc0375f4be87e
“Xeostop1” <stop1@daum.net>
2025-04-01 14:11:05 -0700

    edit eslint4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+// next.config.ts (또는 next.config.js)
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ 빌드 시 ESLint 무시
+  },
 };
 
 export default nextConfig;
