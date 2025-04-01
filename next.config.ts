285d5a33666e684a3456c25f8b1bf6ffa8ef5875
“Xeostop1” <stop1@daum.net>
2025-04-01 14:15:49 -0700

    fix: ignore ESLint errors during build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-// next.config.ts (또는 next.config.js)
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ 빌드 시 ESLint 무시
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
